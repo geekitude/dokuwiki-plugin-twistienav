638749e7a5e76f30b20600c4b081240dcf550a74
Jan Jurzitza <gh@webfreak.org>
2020-03-05 11:50:40 +0100

    Add useheading config back
    useheading was available in previous releases of twistienav, this brings
    it back to enable the old behavior of having IDs in the twistienav menu
    instead of page titles.

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -11,3 +11,4 @@ $lang['twistieMap']         = 'Use map glyph for TwistieNavs showing wiki home i
 $lang['pageIdTrace']        = 'Look for a [<code>pageId</code>] element and try to turn it into a short hierarchical trace with TwistieNavs.';
 $lang['pageIdExtraTwistie'] = 'Attempt to add an extra TwistieNav after [<code>pageId</code>] element that will show a popup with global index.';
 $lang['style']              = 'Use SVG images or Font Awesome glyphs. Font Awesome has to be externally installed (by the template or another plugin).';
+$lang['useheading']         = 'Use first heading title on wiki pages instead of showing IDs.';
